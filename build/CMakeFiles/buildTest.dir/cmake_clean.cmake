1e1e18e08e4d320f70bd7344ac9b312c4bff81b3
PatrickTumulty <pats3mail@gmail.com>
2020-04-29 18:45:19 -0500

    repo

---
+++ build/CMakeFiles/buildTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buildTest.dir/main.cpp.o"
+  "buildTest"
+  "buildTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buildTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
